f2db3569e43f942de04250656f3dd6fc90d0c4d5
deepak2915 <deepaksr2912@gmail.com>
2024-03-17 16:25:05 +0530

    update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module hellohttp/m/v2
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+	http.HandleFunc("/name", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Deepak")
+	})
+	http.HandleFunc("/rollno", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "7376222IT504")
+	})
+
+	fmt.Printf("Server running (port=8080), route: http://localhost:8080/helloworld\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
